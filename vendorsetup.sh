d7567566bb210721440cf72d9999d587d1eacc20
percy-g2 <gahlotpercy@gmail.com>
2015-04-23 21:47:10 +0200

    Surnia: Bring up aosp
    Change-Id: I10804eb6966c9a2e86d3aab30ee23b9d6420631e

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,2 @@
-add_lunch_combo omni_surnia-userdebug
-add_lunch_combo slim_surnia-userdebug
-add_lunch_combo cm_surnia-userdebug
+add_lunch_combo aosp_surnia-userdebug
+add_lunch_combo aosp_surnia-eng
